e4f0ff22fced7cb4b481cadfb578ca4f05428e77
voltan <djvoltan@gmail.com>
2016-02-12 14:20:43 +0330

    Work on event

--- src/Api/Event.php
+++ src/Api/Event.php
@@ -93,18 +93,20 @@ class Event extends AbstractApi
             'slug' => $event['slug'],
         )));
         // Set category
-        $topicList = array();
-        foreach ($event['topics'] as $topic) {
-            $topicList[] = array(
-                'title' => $topic['title'],
-                'url' => Pi::url(Pi::service('url')->assemble('event', array(
-                    'module' => $this->getModule(),
-                    'controller' => 'category',
-                    'slug' => $topic['slug'],
-                ))),
-            );
+        if (isset($event['topics']) && !empty($event['topics'])) {
+            $topicList = array();
+            foreach ($event['topics'] as $topic) {
+                $topicList[] = array(
+                    'title' => $topic['title'],
+                    'url' => Pi::url(Pi::service('url')->assemble('event', array(
+                        'module' => $this->getModule(),
+                        'controller' => 'category',
+                        'slug' => $topic['slug'],
+                    ))),
+                );
+            }
+            $event['topics'] = $topicList;
         }
-        $event['topics'] = $topicList;
         return $event;
     }
 }
