7c20819086c099b9d591e06ee102d7d020f3e2e5
Marco <marcd@nordnet.fr>
2017-01-31 19:43:26 +0100

    bump version

--- config/module.php
+++ config/module.php
@@ -15,7 +15,7 @@ return array(
     'meta' => array(
         'title' => _a('Event'),
         'description' => _a('Manage events'),
-        'version' => '0.4.4',
+        'version' => '0.5.0',
         'license' => 'New BSD',
         'logo' => 'image/logo.png',
         'readme' => 'docs/readme.txt',
