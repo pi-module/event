0edb92a266d3c6138fd444c358bcdeadf92d91b7
voltan <djvoltan@gmail.com>
2017-03-25 18:11:52 +0430

    Work on new load system

--- src/Controller/Front/JsonController.php
+++ src/Controller/Front/JsonController.php
@@ -14,20 +14,203 @@
 namespace Module\Event\Controller\Front;
 
 use Pi;
-use Pi\Mvc\Controller\ActionController;
-use Zend\Db\Sql\Predicate\Expression;
 
 class JsonController extends IndexController
 {
-    public function indexAction()
+    public function searchAction()
     {
-        // Set return
-        $return = array(
-            'website' => Pi::url(),
-            'module' => $this->params('module'),
+        // Get info from url
+        $module = $this->params('module');
+        $page = $this->params('page', 1);
+        $title = $this->params('title');
+        $category = $this->params('category');
+        $tag = $this->params('tag');
+        $favourite = $this->params('favourite');
+        $recommended = $this->params('recommended');
+        $limit = $this->params('limit');
+        $order = $this->params('order');
+
+        // Set has search result
+        $hasSearchResult = true;
+
+        // Clean title
+        if (Pi::service('module')->isActive('search') && isset($title) && !empty($title)) {
+            $title = Pi::api('api', 'search')->parseQuery($title);
+        } elseif (isset($title) && !empty($title)) {
+            $title = _strip($title);
+        } else {
+            $title = '';
+        }
+
+        // Clean params
+        $paramsClean = array();
+        foreach ($_GET as $key => $value) {
+            $key = _strip($key);
+            $value = _strip($value);
+            $paramsClean[$key] = $value;
+        }
+
+        // Get config
+        $config = Pi::service('registry')->config->read($module);
+
+        // Set empty result
+        $result = array(
+            'events' => array(),
+            'paginator' => array(),
+            'condition' => array(),
         );
-        // Set view
-        return $return;
+
+        // Set where link
+        $whereLink = array(
+            'status' => 1,
+            'type' => 'event'
+        );
+        if (!empty($recommended) && $recommended == 1) {
+            $whereLink['recommended'] = 1;
+        }
+
+        // Set page title
+        $pageTitle = __('List of events');
+
+        $order = array('time_publish DESC', 'id DESC');
+        $orderExtra = array('time_start DESC', 'id DESC');
+
+        // Get category information from model
+        if (!empty($category)) {
+            // Get category
+            $category = Pi::api('topic', 'news')->getTopicFull($category, 'slug');
+            // Check category
+            if (!$category || $category['status'] != 1) {
+                return $result;
+            }
+            // Set page title
+            $pageTitle = sprintf(__('List of events on %s category'), $category['title']);
+        }
+
+        // Get tag list
+        if (!empty($tag)) {
+            $eventIDTag = array();
+            // Check favourite
+            if (!Pi::service('module')->isActive('tag')) {
+                return $result;
+            }
+            // Get id from tag module
+            $tagList = Pi::service('tag')->getList($tag, $module);
+            foreach ($tagList as $tagSingle) {
+                $eventIDTag[] = $tagSingle['item'];
+            }
+            // Set header and title
+            $pageTitle = sprintf(__('All events from %s'), $tag);
+        }
+
+        // Get favourite list
+        if (!empty($favourite) && $favourite == 1) {
+            // Check favourite
+            if (!Pi::service('module')->isActive('favourite')) {
+                return $result;
+            }
+            // Get uid
+            $uid = Pi::user()->getId();
+            // Check user
+            if (!$uid) {
+                return $result;
+            }
+            // Get id from favourite module
+            $eventIDFavourite = Pi::api('favourite', 'favourite')->userFavourite($uid, $module);
+            // Set page title
+            $pageTitle = ('All favourite events by you');
+        }
+
+        // Set event ID list
+        $checkTitle = false;
+        $eventIDList = array();
+
+        // Check title from event table
+        if (isset($title) && !empty($title)) {
+            $checkTitle = true;
+            $titles = is_array($title) ? $title : array($title);
+            $columns = array('id');
+            $select = $this->getModel('extra')->select()->columns($columns)->where(function ($where) use ($titles, $recommended) {
+                $whereMain = clone $where;
+                $whereKey = clone $where;
+                $whereMain->equalTo('status', 1);
+                if (!empty($recommended) && $recommended == 1) {
+                    $whereMain->equalTo('recommended', 1);
+                }
+                foreach ($titles as $title) {
+                    $whereKey->like('title', '%' . $title . '%')->and;
+                }
+                $where->andPredicate($whereMain)->andPredicate($whereKey);
+            })->order($orderExtra);
+            $rowset = $this->getModel('extra')->selectWith($select);
+            foreach ($rowset as $row) {
+                $eventIDList[$row->id] = $row->id;
+            }
+        }
+
+        // Set info
+        $event = array();
+        $count = 0;
+
+        $offset = (int)($page - 1) * $config['view_perpage'];
+        $limit = (intval($limit) > 0) ? intval($limit) : intval($config['view_perpage']);
+
+        // Set category on where link
+        if (isset($categoryIDList) && !empty($categoryIDList)) {
+            $whereLink['category'] = $categoryIDList;
+        }
+
+        // Set event on where link from title and attribute
+        if ($checkTitle) {
+            if (!empty($eventIDList)) {
+                $whereLink['story'] = $eventIDList;
+            } else {
+                $hasSearchResult = false;
+            }
+        }
+
+        // Set favourite events on where link
+        if (!empty($favourite) && $favourite == 1 && isset($eventIDFavourite)) {
+            if (isset($whereLink['story']) && !empty($whereLink['story'])) {
+                $whereLink['story'] = array_intersect($eventIDFavourite, $whereLink['story']);
+            } elseif (!empty($whereLink['story'])) {
+                $whereLink['story'] = $eventIDFavourite;
+            } else {
+                $hasSearchResult = false;
+            }
+        }
+
+        // Set tag events on where link
+        if (!empty($tag) && isset($eventIDTag)) {
+            if (isset($whereLink['story']) && !empty($whereLink['story'])) {
+                $whereLink['story'] = array_intersect($eventIDTag, $whereLink['story']);
+            } elseif (!empty($whereLink['story'])) {
+                $whereLink['story'] = $eventIDTag;
+            } else {
+                $hasSearchResult = false;
+            }
+        }
+
+        // Check has Search Result
+        if ($hasSearchResult) {
+            $event = Pi::api('event', 'event')->getEventList($whereLink, $order, $offset, $limit, 'full', 'link');
+            $count = Pi::api('api', 'news')->getStoryCount($whereLink, 'link');
+        }
+
+        // Set result
+        $result = array(
+            'events' => $event,
+            'paginator' => array(
+                'count' => $count,
+                'limit' => intval($config['view_perpage']),
+                'page' => $page,
+            ),
+            'condition' => array(
+                'title' => $pageTitle,
+            ),
+        );
+
+        return $result;
     }
 
     public function filterIndexAction()
